ad01827714278daa45987e2650f58a7cebbfca78
Timothee Boussus <timothee.boussus@gmail.com>
2019-12-22 17:58:11 +0100

    Added nordvpn optimisation

--- deluge/scripts/root/openvpn.sh
+++ deluge/scripts/root/openvpn.sh
@@ -6,8 +6,18 @@ then
     echo "$VPN_USER" > /config/openvpn/credentials.conf
     echo "$VPN_PASS" >> /config/openvpn/credentials.conf
 
+    config_file="/config/openvpn/config.ovpn"
+
+    if [[ "$VPN_PROVIDER" == "NORDVPN" ]]
+    then
+        vpn_server=$(curl --silent "https://api.nordvpn.com/v1/servers/recommendations?filters\[servers_groups\]\[identifier\]=legacy_p2p&limit=1" | jq '.[].hostname' --raw-ouput)
+        echo "NordVPN provider, will use ${vpn_server} instead of config.ovpn"
+        config_file="/config/openvpn/${vpn_server}.ovpn"
+        curl "https://downloads.nordcdn.com/configs/files/ovpn_legacy/servers/${vpn_server}.udp1194.ovpn" > $config_file
+    fi
+
     openvpn                                                 \
-    --config /config/openvpn/config.ovpn                    \
+    --config "$config_file"                                 \
     --auth-user-pass /config/openvpn/credentials.conf       \
     --script-security 2 --up /root/vpnup.sh                 \
     --setenv LOCAL_NETWORK "${LOCAL_NETWORK}"
